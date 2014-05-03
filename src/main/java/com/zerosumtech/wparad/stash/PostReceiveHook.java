feb5a10b842878514246e8f536ab7328b5badbbc
Warren Parad <wparad@gmail.com>
2014-05-03 13:15:12 -0400

    Add settings lookup.

--- src/main/java/com/zerosumtech/wparad/stash/PostReceiveHook.java
+++ src/main/java/com/zerosumtech/wparad/stash/PostReceiveHook.java
@@ -1,9 +1,14 @@
 package com.zerosumtech.wparad.stash;
 
+import com.atlassian.event.api.EventListener;
+import com.atlassian.stash.event.pull.PullRequestOpenedEvent;
+import com.atlassian.stash.event.pull.PullRequestReopenedEvent;
 import com.atlassian.stash.event.pull.PullRequestRescopedEvent;
+import com.atlassian.stash.event.RepositoryRefsChangedEvent;
 import com.atlassian.stash.hook.repository.AsyncPostReceiveRepositoryHook;
 import com.atlassian.stash.hook.repository.RepositoryHookContext;
-import com.atlassian.stash.nav.NavBuilder;
+import com.atlassian.stash.hook.repository.RepositoryHookService;
+import com.atlassian.stash.pull.PullRequest;
 import com.atlassian.stash.pull.PullRequestRef;
 import com.atlassian.stash.repository.RefChange;
 import com.atlassian.stash.repository.RefChangeType;
@@ -11,10 +16,8 @@ import com.atlassian.stash.repository.Repository;
 import com.atlassian.stash.setting.RepositorySettingsValidator;
 import com.atlassian.stash.setting.Settings;
 import com.atlassian.stash.setting.SettingsValidationErrors;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.atlassian.stash.user.Permission;
+import com.atlassian.stash.user.PermissionValidationService;
 
 import java.io.DataOutputStream;
 import java.io.UnsupportedEncodingException;
@@ -23,103 +26,61 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.Collection;
 
-import com.atlassian.event.api.EventListener;
-import com.atlassian.event.api.EventListener;
-import com.atlassian.stash.event.RepositoryRefsChangedEvent;
-import com.atlassian.stash.event.pull.PullRequestOpenedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class PostReceieveHook implements AsyncPostReceiveRepositoryHook, RepositorySettingsValidator 
+public class PostReceiveHook implements AsyncPostReceiveRepositoryHook, RepositorySettingsValidator 
 {
-
-    private static final Logger log = LoggerFactory.getLogger(PostReceieveHook.class);
-    private static final String REFS_HEADS = "refs/heads/";  //TODO: add in pull requests here, I don't even think this matters
-
-    public PostReceieveHook(NavBuilder navBuilder) {
-		String cloneUrl = navBuilder.repo(context.getRepository()).clone(context.getRepository().getScmId()).buildAbsolute();
-		String branches = urlEncode(Joiner.on(",").join(b));
-		//return context.getSettings().getString(CONFIG_URL, "").replace("$URL", urlEncode(cloneUrl)).replace("$BRANCHES", branches);
+	private static final String PLUGIN_KEY = "com.zerosumtech.wparad.stash-stash-http-request-trigger";
+	private static final String HOOK_KEY = "postReceiveHook";
+    private static final Logger log = LoggerFactory.getLogger(PostReceiveHook.class);
+    private final PermissionValidationService permissionValidationService;
+    private final RepositoryHookService repositoryHookService; 
+    
+    public PostReceiveHook(PermissionValidationService permissionValidationService, RepositoryHookService repositoryHookService) 
+    {
+    	this.permissionValidationService = permissionValidationService;
+    	this.repositoryHookService = repositoryHookService;
     }
 
     @Override
     public void postReceive(RepositoryHookContext context, Collection<RefChange> refChanges) 
     {
-		/*
-		 * Don't need to do this because it's in the repository change event
-		 * // NOTE: Need to pass this down into postChange
-        String url = context.getSettings().getString("url");
-
-        Iterable<RefChange> updatedRefs = Iterables.filter(refChanges, new Predicate<RefChange>() 
-        {
-            @Override
-            public boolean apply(RefChange input) {
-                // We only care about non-deleted branches //TODO: probably care about more things than just heads here
-                return input.getType() != RefChangeType.DELETE && input.getRefId().startsWith(REFS_HEADS);
-            }
-        });
-
-        for (RefChange refChange : updatedRefs) 
-        {
-            log.info("Ref changed: " + refChange.getRefId());
-            String ref = refChange.getRefId().replace(REFS_HEADS, "");
-
-            String sha = refChange.getToHash();
-            postChange(ref, sha);
-        }
-		 * */
+		//Don't need to do this because the event triggers will handle the receive.
     }
 
-  /**
-   * Event listener that is notified of both pull request merges and push events
-   * @param event The pull request event
-   */
     @EventListener
     public void onRefsChangedEvent(RepositoryRefsChangedEvent event) 
-  {    
-	String user = (event.getUser() != null) ? event.getUser().getName() : null;
-	EventContext context = new EventContext(event, event.getRepository(), user);
-	
-	if (filterChain.shouldDeliverNotification(context))
-	  postChange(fromRef.getId(), fromRef.getLatestChangeset());
-	  
-  }
-
-    @EventListener
-    public void pullRequestOpened(PullRequestOpenedEvent event) 
     {
-        EventContext context = new EventContext(event, 
-        event.getPullRequest().getToRef().getRepository(), 
-        event.getUser().getName());
-    
-		if (filterChain.shouldDeliverNotification(context))
-		  notifier.notifyBackground(context.getRepository());
-		  
-        PullRequestRef fromRef = event.getPullRequest().getFromRef();
-
-        postChange(fromRef.getId(), fromRef.getLatestChangeset());
+    	Repository repository = event.getRepository();
+    	for(RefChange refChange : event.getRefChanges())
+    	{
+    		if(refChange.getType() == RefChangeType.DELETE) {continue;}
+    		postChange(repository, refChange.getRefId(), refChange.getToHash());
+    	}
     }
 
     @EventListener
-    public void pullRequestRescoped(PullRequestRescopedEvent event) 
-    {
-		EventContext context = new EventContext(event, 
-        event.getPullRequest().getToRef().getRepository(), 
-        event.getUser().getName());
+    public void pullRequestOpened(PullRequestOpenedEvent event) { HandlePullRequestEvent(event.getPullRequest()); }
     
-		if (filterChain.shouldDeliverNotification(context))
-		  notifier.notifyBackground(context.getRepository());
-		  
-        PullRequestRef fromRef = event.getPullRequest().getFromRef();
+    @EventListener
+    public void onPullRequestReopened(PullRequestReopenedEvent event) { HandlePullRequestEvent(event.getPullRequest()); }
 
-        if (!event.getPreviousFromHash().equals(fromRef.getLatestChangeset())) {
-            postChange(fromRef.getId(), fromRef.getLatestChangeset());
-        }
-    }
+    @EventListener
+    public void pullRequestRescoped(PullRequestRescopedEvent event) { HandlePullRequestEvent(event.getPullRequest()); }
 
-    private void postChange(String ref, String sha) 
+    private void HandlePullRequestEvent(PullRequest pullRequest)
     {
-        // Redefine this so it pulls from context
-        String baseUrl = "http://localhost:3000/repositories/7/build-ref";
-        String urlParams = "ref=" + urlEncode(ref) + "&sha=" + urlEncode(sha);
+    	Repository repository = pullRequest.getToRef().getRepository();
+    	PullRequestRef ref = pullRequest.getFromRef();
+		postChange(repository, ref.getId(), ref.getLatestChangeset());
+    }
+    private void postChange(Repository repository, String ref, String sha) 
+    {
+    	permissionValidationService.validateForRepository(repository, Permission.REPO_READ);
+    	Settings settings = repositoryHookService.getSettings(repository, PLUGIN_KEY + ":" + HOOK_KEY);
+    	String baseUrl = settings.getString("url");
+        String urlParams = "STASH_REF=" + urlEncode(ref) + "&STASH_SHA=" + urlEncode(sha);
 
         post(baseUrl, urlParams);
     }
@@ -140,25 +101,18 @@ public class PostReceieveHook implements AsyncPostReceiveRepositoryHook, Reposit
             wr.close();
 
             conn.getInputStream().close();
-
         } 
         catch (Exception e) { log.error("Error in post", e); }
     }
 
-
     private static String urlEncode(String string) {
-        try {
-            return URLEncoder.encode(string, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
+        try { return URLEncoder.encode(string, "UTF-8"); } 
+        catch (UnsupportedEncodingException e) { throw new RuntimeException(e); }
     }
 
     @Override
     public void validate(Settings settings, SettingsValidationErrors errors, Repository repository) {
         String url = settings.getString("url");
-        if (url == null || url.trim().isEmpty()) {
-            errors.addFieldError("url", "URL field is blank, please supply one");
-        }
+        if (url == null || url.trim().isEmpty())  { errors.addFieldError("url", "URL field is blank, please supply one"); }
     }
 }
